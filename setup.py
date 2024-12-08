7c1250252ad98aadb756d46db041c32b6f2f5777
Hank Liao <harry90911@gmail.com>
2024-12-08 18:40:08 +0800

    Initialize project

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",  # 版本號
     description="A Python package for Google Sheets integration using OAuth2 and Pandas.",
     author="hank.liao",
-    author_email="hank.liao@ichef.com.tw",
+    author_email="harry90911@gmail.com",
     # url="https://github.com/your_username/gsheet_handler",  # GitHub 項目地址（可選）
     packages=find_packages(),  # 自動查找包
     install_requires=[
